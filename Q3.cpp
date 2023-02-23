0b29550fc2bcf44961a1567f4963632295e72bbb
PRIYUSH KHOBRAGADE <124695270+Priyush02K@users.noreply.github.com>
2023-02-23 13:48:07 +0530

    Add files via upload

---
+++ Q3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     int length=35;
+     int breadth=45;
+ cout <<"Lenght "<< length << endl;
+ cout<< "Breadth" << breadth << endl;
+
+ int area = length*breadth;
+    cout << "Area is: " << area << endl;
+    return 0;
+}
---
+++ Q4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int x;
+     cout<< "Entre The No :";
+     cin >> x;
+     int cube;
+     cout <<"Number is"<<x<<endl;
+     cout << "Cube is the number is:"<<x*x*x<<endl;
+     return 0;
+}
